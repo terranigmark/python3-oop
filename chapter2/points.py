1c301911a452faacd3049595be648f89868925c2
Hector Vega <linnk99@gmail.com>
2021-01-16 10:17:10 -0500

    Point class methods expanded with distance calculation

--- chapter2/points.py
+++ chapter2/points.py
@@ -1,15 +1,32 @@
+import math
+
+
 class Point:
-    pass
+    def move(self, x, y):
+        self.x = x
+        self.y = y
 
-p1 = Point()
-p2 = Point()
+    def reset(self):
+        self.move(0, 0)
 
-# Dot notation
-p1.x = 5
-p1.y = 4
 
-p2.x = 3
-p2.y = 6
+    def calculate_distance(self, other_point):
+        return math.sqrt(
+            (self.x - other_point.x) ** 2
+            + (self.y - other_point.y) ** 2
+        )
 
-print(p1.x, p1.y)
-print(p2.x, p2.y)
+# How to use it:
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(5, 0)
+
+print(point2.calculate_distance(point1))
+assert point2.calculate_distance(point1) == point1.calculate_distance(point2
+)
+
+point1.move(3, 4)
+print(point1.calculate_distance(point2))
+print(point1.calculate_distance(point1))
