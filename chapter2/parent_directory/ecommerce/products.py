c5d581bfbeb23dfaffff1059a3f5b4c3f6e18635
Hector Vega <linnk99@gmail.com>
2021-01-17 20:51:01 -0500

    Absolute import modules exampled added

---
+++ chapter2/parent_directory/ecommerce/products.py
@@ -0,0 +1,8 @@
+from database import Database as DB
+from database import Query
+
+class Product:
+    pass
+
+db = DB()
+# Do queries on db
---
+++ chapter2/parent_directory/main.py
@@ -0,0 +1,8 @@
+import ecommerce.products
+product = ecommerce.products.Product()
+
+from ecommerce.products import Product
+product = Product()
+
+from ecommerce import products
+product = products.Product()
