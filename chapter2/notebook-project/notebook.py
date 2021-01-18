04b401e18bc2a6ec1fa4ac9e18a45ec7dfd634b3
Hector Vega <linnk99@gmail.com>
2021-01-18 12:06:04 -0500

    Notebook CRUD created and solved search notes bug about ids

---
+++ chapter2/notebook-project/notebook.py
@@ -0,0 +1,67 @@
+import datetime
+
+# Store the next available id for all new notes
+last_id = 0
+
+class Note:
+    """
+    Represents a note in the notebook.
+    Match against a string in searches and store tags for each note.
+    """
+
+    def __init__(self, memo, tags=""):
+        """Initialize a note with memo and optional space-separated tags.
+        Automatically set the notes's creation date and a unique id.
+
+        Args:
+            memo ([type]): [description]
+            tags (str, optional): [description]. Defaults to "".
+        """
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self, filter):
+        """Determine if this note matches the filter text.
+        Return True if it matches, False otherwise.
+
+        Search is case sensitive and matches both text and tags.
+
+        Args:
+            filter ([type]): [description]
+        """
+        return filter in self.memo or filter in self.tags
+
+class Notebook:
+    """Represents a collection of notes that can be tagged, modified, and searched."""
+
+    def __init__(self):
+        """Initialize a notebook with an empty list."""
+        self.notes = []
+
+    def new_note(self, memo, tags=""):
+        """Creates a new note and add it to the list."""
+        self.notes.append(Note(memo, tags))
+
+    def _find_note(self, note_id):
+        """Locate the note with the given id."""
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        
+        return None
+
+    def modify_memo(self, note_id, memo):
+        """Find the note with the given id and change its memo to the given value."""
+        self._find_note(note_id).memo == memo
+
+    def modify_tags(self, note_id, tags):
+        """Find the note with the give id and change its tags to the given value."""
+        self._find_note(note_id).tags == tags
+
+    def search(self, filter):
+        """Find all note that match the give filter string."""
+        return [note for note in self.notes if note.match(filter)]
