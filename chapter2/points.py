843f7ffe52c6e41b7a91e37b8c61ff5a1847a450
Hector Vega <linnk99@gmail.com>
2021-01-17 20:21:12 -0500

    Docstrings added to the Point class

--- chapter2/points.py
+++ chapter2/points.py
@@ -2,18 +2,42 @@ import math
 
 
 class Point:
+    "Represents a point in two-dimensional geometric coordinates."
     def __init__(self, x=0, y=0):
+        """Initialize the position of a new point.
+        The x and y coordinates can be specified. If they aren't,
+        the point defaults to the origin.
+
+        Args:
+            x (int, optional): [description]. Defaults to 0.
+            y (int, optional): [description]. Defaults to 0.
+        """
         self.move(x, y)
 
     def move(self, x, y):
+        "Moves the point to a new location in 2D space."
         self.x = x
         self.y = y
 
     def reset(self):
+        "Reset the point back to the geometric origin: 0, 0."
         self.move(0, 0)
 
 
     def calculate_distance(self, other_point):
+        """Calculate the distance from this point to a second
+        point passed as a parameter.
+
+        This funtion uses the Pythagorean Theorem to calculate the
+        distance between the two points.
+        The distance is returned as a float.
+
+        Args:
+            other_point ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         return math.sqrt(
             (self.x - other_point.x) ** 2
             + (self.y - other_point.y) ** 2
